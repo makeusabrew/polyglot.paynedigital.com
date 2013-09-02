a0cf3f8e7e2db7a94504ae8e3b84fedb08d74295
Nick Payne <nick@kurai.co.uk>
2013-09-02 18:49:17 +0100

    install php5-fpm

--- conf/puppet/manifests/default.pp
+++ conf/puppet/manifests/default.pp
@@ -1,44 +1,53 @@
-exec { 'apt-get update': 
-    command => '/usr/bin/apt-get update'
+# general packages & services
+
+exec { "update":
+    command => '/usr/bin/apt-get update',
 }
 
 exec { 'install-properties':
     command => "/usr/bin/apt-get install -y python-software-properties",
-    require => Exec["apt-get update"]
+    require => Exec["update"],
 }
 
 exec { "update-nginx":
     command => "/usr/bin/add-apt-repository -y ppa:nginx/stable && /usr/bin/apt-get update",
-    require => Exec["install-properties"]
+    require => Exec["install-properties"],
 }
 
 package { 'nginx': 
     ensure => present,
-    require => Exec['update-nginx']
+    require => Exec['update-nginx'],
 }
 
 service { 'nginx':
     ensure => running,
-    require => Package['nginx']
+    require => Package['nginx'],
 }
 
 # golang version
 $version = "1.1.2"
 
 exec { "download-golang":
-    command => "/usr/bin/wget -O /usr/local/src/go$version.linux-amd64.tar.gz https://go.googlecode.com/files/go$version.linux-amd64.tar.gz",
-    creates => "/usr/local/src/go$version.linux-amd64.tar.gz"
+    command => "/usr/bin/wget -O /usr/local/src/go${version}.linux-amd64.tar.gz https://go.googlecode.com/files/go${version}.linux-amd64.tar.gz",
+    creates => "/usr/local/src/go${version}.linux-amd64.tar.gz",
 }
 
 exec { "unarchive-golang-tools":
-    command => "/bin/tar -C /usr/local -xzf /usr/local/src/go$version.linux-amd64.tar.gz",
-    require => Exec["download-golang"]
+    command => "/bin/tar -C /usr/local -xzf /usr/local/src/go${version}.linux-amd64.tar.gz",
+    require => Exec["download-golang"],
 }
 
 exec { "setup-path":
-    command => "/bin/echo 'export PATH=/vagrant/bin:/usr/local/go/bin:\$PATH' >> /home/vagrant/.profile"
+    command => "/bin/echo 'export PATH=/vagrant/bin:/usr/local/go/bin:\$PATH' >> /home/vagrant/.profile",
 }
 
 exec { "setup-workspace":
-    command => "/bin/echo 'export GOPATH=/vagrant' >> /home/vagrant/.profile"
+    command => "/bin/echo 'export GOPATH=/vagrant' >> /home/vagrant/.profile",
+}
+
+# php
+
+package { "php5-fpm":
+    ensure  => present,
+    require => Exec["update"],
 }
